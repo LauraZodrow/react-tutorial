cd5c3531d4790c14b08220aa4f0330f4b2771e31
Laura Zodrow <laura.zodrow@readytalk.com>
2016-09-08 11:58:50 -0600

    first commit

---
+++ public/scripts/example.js
@@ -0,0 +1,82 @@
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only. Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// var data = [
+// 	{
+// 		id: 1, 
+// 		author: "Pete Hung", 
+// 		text: "This is one comment"
+// 	},
+// 	{
+// 		id: 2, 
+// 		author: "Jordan Walke", 
+// 		text: "This is *another* comment"
+// 	}
+// ]
+
+var CommentBox = React.createClass({
+	render:function(){
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList data={this.props.data} />
+				<CommentForm />
+			</div>
+		);
+	}
+});
+
+
+var CommentList = React.createClass({
+	render:function(){
+		var commentNodes = this.props.data.map(function(comment){
+			return (
+				<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+				</Comment>
+			);
+		});
+		return (
+			<div className="commentList">
+				{commentNodes}
+			</div>
+		)
+	}
+});
+
+var CommentForm = React.createClass({
+	render:function(){
+		return (
+			<div className="commentForm">
+				Hello,World! I am a CommentForm.
+			</div>
+		);
+	}
+});
+
+var Comment = React.createClass({
+	render:function(){
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(
+	<CommentBox url="/api/comments" />,
+	document.getElementById('content')
+);
